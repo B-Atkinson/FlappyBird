3c57c9b4501ff034289988c71b1047f53870ccd0
B-Atkinson <brian.atkinson@nps.com>
2022-01-10 11:03:11 -0800

    testing shell script loops

--- human.sh
+++ human.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#SBATCH --job-name=daniel.deridder
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:1
-#SBATCH --name=human
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=64G
-#SBATCH --time=04:00:00
-#SBATCH --output=titans-out-%j.txt
 
 . /etc/profile
 
@@ -14,12 +6,21 @@ module load lang/miniconda3/4.8.3
 
 source activate thesis
 
-python trainer/task.py \
---model_dir="/data/beards/CS4321/Team_DeRidderAtkinson/code/trainer/models/mnist_tests$(date +%Y-%m-%d_%H-%M-%S)/" \
---model_type="fully_connected" \
---num_epochs=15 \
---batch_size=64 \
---num_classes=10 \
---eval_metrics="accuracy" \
---optimizer="SGD" \
---callback_list="checkpoint, csv_log"
+for i in 1 2 3 4
+
+do
+#SBATCH --job-name=birds_arent_real
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --name=human
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=64G
+#SBATCH --time=04:00:00
+#SBATCH --output=test-%j.txt
+
+
+
+python FB_Main.py \
+--num_episodes=$i * 10
+
+done
