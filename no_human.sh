537b008977530f4bb543e64dafb223c26e713d56
B-Atkinson <73620879+B-Atkinson@users.noreply.github.com>
2022-02-11 11:08:12 -0800

    Update no_human.sh

--- no_human.sh
+++ no_human.sh
@@ -4,9 +4,9 @@
 #SBATCH --partition=beards
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
-#SBATCH --mem=64G
-#SBATCH --time=240:00:00
-#SBATCH --output=Projection_Heuristic/no_human_24.txt
+#SBATCH --mem=32G
+#SBATCH --time=96:00:00
+#SBATCH --output=pure_learning_.01_%j.txt
 
 . /etc/profile
 
@@ -15,17 +15,17 @@ module load lang/miniconda3 lib/cuda/11.5
 source activate py38
 
 python FB_Main.py \
---num_episodes=500000 \
+--num_episodes=200000 \
 --seed=42 \
 --loss_reward=-5 \
 --save_stats=200 \
 --render=false \
 --gamma=0.99 \
---learning_rate=0.0001 \
+--learning_rate=0.01 \
 --decay_rate=0.99 \
 --batch_size=200 \
 --human=false \
 --hidden_save_rate=400 \
 --continue_training=false \
 --checkpoint_path=null \
---output_dir=/home/brian.atkinson/thesis/data
+--output_dir=/home/brian.atkinson/thesis/data/Learning_0.01
