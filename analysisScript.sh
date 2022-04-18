b2d950deb696beb4c344e80dca560bd609e4b149
Brian R. Atkinson <brian.atkinson@submit-0.hamming.cluster>
2022-04-18 11:46:34 -0700

    added analysis script

---
+++ analysisScript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=analysis
+#SBATCH --nodes=3 
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=16G
+#SBATCH --time=00:00:20
+#SBATCH --output=analysis.txt
+
+. /etc/profile
+
+module load lang/miniconda3
+
+source activate py38
+
+python analysis.py \
+--rootDirectory=/home/brian.atkinson/thesis/data
