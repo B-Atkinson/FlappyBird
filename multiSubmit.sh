a39774d67ce8381692547e32ec12697033cfd4d0
Brian R. Atkinson <brian.atkinson@submit-0.hamming.cluster>
2022-06-01 11:07:53 -0700

    last test

--- multiSubmit.sh
+++ multiSubmit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-OUTPUT=/home/brian.atkinson/thesis/data/noGPU/L2_norm
+OUTPUT=/home/brian.atkinson/thesis/data/noGPU/L2_norm_large
 echo -e "saving experiment to:\n$OUTPUT\n"
 
 JOB=1
 for HUMAN in false
 do
-    for SEED in 1 #2
+    for SEED in 1 3
     do
         for LEAKY in false #true
         do
@@ -13,11 +13,11 @@ do
             do
                 for L2 in true
                 do
-                    for L2Constant in .0001 #.00001 .000001
+                    for L2Constant in 1 10 100
                     do    
                         sbatch --job-name=$JOB\
                         --export=ALL,HUMAN=$HUMAN,SEED=$SEED,LEAKY=$LEAKY,OUTPUT=$OUTPUT,INIT=$INIT,L2=$L2,L2Constant=$L2Constant \
-                        --output=/home/brian.atkinson/thesis/FlappyBird/text_files/H$HUMAN.S$SEED.L$LEAKY.init$INIT.L2$L2.txt \
+                        --output=/home/brian.atkinson/thesis/FlappyBird/text_files/plus_H$HUMAN.S$SEED.L$LEAKY.init$INIT.L2$L2.txt \
                         mainScript.sh
                         let JOB=JOB+1
                     done
