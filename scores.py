196403bb1054a41dc9436a05c2efb0962b75868c
Brian R. Atkinson <brian.atkinson@submit-0.hamming.cluster>
2022-06-15 08:49:45 -0700

    use prints to combine into single .txt file

--- scores.py
+++ scores.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import numpy as np
 
-rootDir = pathlib.Path('/home/brian.atkinson/thesis/data/noGPU/noRMSprop')
+rootDir = pathlib.Path('/home/brian.atkinson/thesis/data/noGPU/hybrid')
 humanScores,baseScores = [],[]
 bestBaseScore,bestBaseDir,bestHumScore,bestHumDir  = 0, None,0, None
 worstBaseScore,worstBaseDir,worstHumScore,worstHumDir = 100, None,100, None
@@ -79,16 +79,24 @@ humanArray = np.array(humanScores)
 baseArray = np.array(baseScores)
 
 
-names = {'hybridResults.txt':hybDict,'weightInitResults.txt':activDict,'weightInitResults.txt':weightDict}
+names = {'hybridResults.txt':hybDict,'activationResults.txt':activDict,'weightInitResults.txt':weightDict}
 
 for name in names.keys():
     d= names[name]
-    with open(name,'w') as fd:
-        fd.write('human avg:{}    base avg:{}\n'.format(np.mean(humanArray),np.mean(baseArray)))
-        for k in d.keys():
-            fd.write('\n{} Stats\n'.format(k))
-            scoreArray = np.array(d[k]['scores'])
-            fd.write('avg:{}   median:{}\n'.format(np.mean(scoreArray),np.median(scoreArray)))
-            for k2 in d[k].keys():
-                if k2 != 'scores':
-                    fd.write('{}:{}\n'.format(k2,d[k][k2]))
+    # with open(name,'w') as fd:
+    #     fd.write('human avg:{}    base avg:{}\n'.format(np.mean(humanArray),np.mean(baseArray)))
+        # for k in d.keys():
+        #     fd.write('\n{} Stats\n'.format(k))
+        #     scoreArray = np.array(d[k]['scores'])
+        #     fd.write('avg:{}   median:{}\n'.format(np.mean(scoreArray),np.median(scoreArray)))
+        #     for k2 in d[k].keys():
+        #         if k2 != 'scores':
+        #             fd.write('{}:{}\n'.format(k2,d[k][k2]))
+
+    for k in d.keys():
+        print('\n{} Stats'.format(k))
+        scoreArray = np.array(d[k]['scores'])
+        print('avg:{}   median:{}'.format(np.mean(scoreArray),np.median(scoreArray)))
+        for k2 in d[k].keys():
+            if k2 != 'scores':
+                print('{}:{}'.format(k2,d[k][k2]))
